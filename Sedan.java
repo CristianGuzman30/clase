9c3a7d65b8b5800dbb4d2bd7b9cafd1e6206efca
Cristian Guzman <cristian.guzmanr@uniagustiniana.edu.co>
2023-02-28 19:41:21 -0500

    se agregaron 6 archivos, 3 clases y 3 codigos fuente

---
+++ Sedan.java
@@ -0,0 +1,13 @@
+public class Sedan extends Carros{
+
+	Sedan(){
+	//Esto es el metodo constructor
+	super.setVelocidad(30);	
+	}
+
+	Sedan(int a){
+	//Esto es el metodo constructor
+	super.setVelocidad(a);
+	}
+	
+}
